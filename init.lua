ee1cde396d2c3bb1b1af4ac1e1d654171e024e26
Jaudat Syed <sjh.jaudat@gmail.com>
2024-04-22 00:28:46 -0400

    updated colorscheme

--- init.lua
+++ init.lua
@@ -387,6 +387,8 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = { layout_config = { horizontal = { preview_cutoff = 0 } } },
+        pickers = { colorscheme = { enable_preview = true } },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -845,7 +847,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
