f012a5d8580834e2831ddfb931cb541bfcda2791
Jaudat Syed <sjh.jaudat@gmail.com>
2024-06-13 21:58:09 -0400

    added context aware comment ability for jsx files

--- init.lua
+++ init.lua
@@ -243,6 +243,26 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
+  --  This is equivalent to:
+  --    require('Comment').setup({})
+
+  -- "gc" to comment visual regions/lines
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
+
+  {
+
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {},
+  },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
