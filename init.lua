741227b10ba42f142f601c0667b0a507df3c501d
Jaudat Syed <sjh.jaudat@gmail.com>
2025-01-03 23:16:24 -0800

    updated colorscheme and fixed oil.nvim config

--- init.lua
+++ init.lua
@@ -288,51 +288,52 @@ require('lazy').setup({
     'stevearc/oil.nvim',
     opts = {},
     config = function()
-      require('oil').setup()
+      require('oil').setup {
+        -- Optional dependencies
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+          -- This function defines what is considered a "hidden" file
+          is_hidden_file = function(name, bufnr)
+            return vim.startswith(name, '.')
+          end,
+          -- This function defines what will never be shown, even when `show_hidden` is set
+          is_always_hidden = function(name, bufnr)
+            return false
+          end,
+          -- Sort file names in a more intuitive order for humans. Is less performant,
+          -- so you may want to set to false if you work with large directories.
+          natural_order = true,
+          -- Sort file and directory names case insensitive
+          case_insensitive = false,
+          sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { 'type', 'asc' },
+            { 'name', 'asc' },
+          },
+        },
+        keymaps = {
+          ['g?'] = 'actions.show_help',
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+          ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+          ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+          ['<C-p>'] = 'actions.preview',
+          ['<C-c>'] = 'actions.close',
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = 'actions.parent',
+          ['_'] = 'actions.open_cwd',
+          ['`'] = 'actions.cd',
+          ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory', mode = 'n' },
+          ['gs'] = 'actions.change_sort',
+          ['gx'] = 'actions.open_external',
+          ['g.'] = 'actions.toggle_hidden',
+          ['g\\'] = 'actions.toggle_trash',
+        },
+      }
     end,
-    -- Optional dependencies
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    view_options = {
-      -- Show files and directories that start with "."
-      show_hidden = true,
-      -- This function defines what is considered a "hidden" file
-      is_hidden_file = function(name, bufnr)
-        return vim.startswith(name, '.')
-      end,
-      -- This function defines what will never be shown, even when `show_hidden` is set
-      is_always_hidden = function(name, bufnr)
-        return false
-      end,
-      -- Sort file names in a more intuitive order for humans. Is less performant,
-      -- so you may want to set to false if you work with large directories.
-      natural_order = true,
-      -- Sort file and directory names case insensitive
-      case_insensitive = false,
-      sort = {
-        -- sort order can be "asc" or "desc"
-        -- see :help oil-columns to see which columns are sortable
-        { 'type', 'asc' },
-        { 'name', 'asc' },
-      },
-    },
-    keymaps = {
-      ['g?'] = 'actions.show_help',
-      ['<CR>'] = 'actions.select',
-      ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-      ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
-      ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
-      ['<C-p>'] = 'actions.preview',
-      ['<C-c>'] = 'actions.close',
-      ['<C-l>'] = 'actions.refresh',
-      ['-'] = 'actions.parent',
-      ['_'] = 'actions.open_cwd',
-      ['`'] = 'actions.cd',
-      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory', mode = 'n' },
-      ['gs'] = 'actions.change_sort',
-      ['gx'] = 'actions.open_external',
-      ['g.'] = 'actions.toggle_hidden',
-      ['g\\'] = 'actions.toggle_trash',
-    },
   },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -468,7 +469,10 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
-        defaults = { layout_config = { horizontal = { preview_cutoff = 0 } } },
+        defaults = {
+          layout_config = { horizontal = { preview_cutoff = 0 } },
+          hidden = true,
+        },
         pickers = { colorscheme = { enable_preview = true } },
         extensions = {
           ['ui-select'] = {
@@ -926,13 +930,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
