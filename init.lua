26468b7805c56ccacdec33484e03855aca8b8217
Jaudat Syed <jaudats@remitly.com>
2025-02-14 22:03:14 -0800

    added eslint linting errors for js,jsx,ts,tsx files in neovim

--- init.lua
+++ init.lua
@@ -757,6 +757,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'eslint_d', -- Used for JavaScript/TypeScript linting
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -1058,7 +1059,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
